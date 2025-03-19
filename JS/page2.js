1f41e714a105a395a58c897cd4dfce28f35e3d04
dianamontero7 <dianamontero38@gmail.com>
2025-03-19 11:39:31 -0500

    .

---
+++ JS/page2.js
@@ -0,0 +1,13 @@
+debugger;
+console.log("hey ya'll");
+
+let x = "Bugs Bunny";
+
+x += ": What's up, Doc!?!"
+
+function characterSays(character) {
+    console.log(character);
+}
+
+
+characterSays(x);
